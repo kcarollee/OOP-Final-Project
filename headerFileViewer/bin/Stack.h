d36c338eca9a3cd5bf53ae2bc7b5bf21710c3f28
dukwon lee <kcarollee31693@gmail.com>
2019-06-13 00:37:50 +0900

    Final push

--- headerFileViewer/bin/Stack.h
+++ headerFileViewer/bin/Stack.h
@@ -1,40 +1,27 @@
-           class                Stack  {
-
-
-
-
+class Stack{
 public:
     Stack(int s  = 10); //default constructor (stack size 10)
-
-
-    ~Stack(){delete[] ptr;} // destructor
-
-
+    ~Stack()   { delete[] ptr;} // destructor
     void push(int v); // push an element onto the stack
-
-
-
     int pop(); // pop an element off the stack
-
-
-protected:
-
-
-    bool isEmpty(); // determine whether Stack is empty
-
-
-
-    bool isFull(); // determine whether Stack is full
 private:
-    int size; // # of elements in the stack
+    int size; // # of elements in the stacks
     int top; // location of the top element
     int *ptr // pointer to the stack
-}; // end class stack
 
-Stack::Stack(int a){
-    size = s > 0 ? s : 10;
-    top = -1;
-    ptr = new int[size];
+   
+protected:
+    bool isEmpty(); // determine whether Stack is empty
+    bool isFull(); // determine whether Stack is full
+   
+};
+// end class stack
+
+int Stack::pop(){
+     if (!isEmpty()){
+         return ptr[top--];
+     }
+     exit(1);
 }
 
 void Stack::push(int v){
@@ -43,19 +30,20 @@ void Stack::push(int v){
     } //  end if
 }
 
-int Stack::pop(){
-    if (!isEmpty()){
-        return ptr[top--];
-    }
-    exit(1);
+Stack::Stack(int  a){
+    size = s > 0 ? s : 10;
+    top = -1;
+    ptr = new int[size];
 }
 
 bool Stack::isFull(){
     if (top >= size - 1) return true;
-    else return false;
+    else  return false;
 }
 
 bool Stack::isEmpty(){
     if (top == -1) return true;
     else return false;
 }
+
+
